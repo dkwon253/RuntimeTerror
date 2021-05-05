76a73b17123c9f7f84c161dc220e01d37a698d12
chenjr16 <chenjr16@gmail.com>
2021-05-05 10:35:45 -0700

    Refactored text parser to read JSON file for verb synonyms.

--- Game/src/com/runtimeterror/textparser/Parser.java
+++ Game/src/com/runtimeterror/textparser/Parser.java
@@ -1,19 +1,43 @@
 package com.runtimeterror.textparser;
-import static com.runtimeterror.textparser.Verbs.*;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
 
 public class Parser {
 
-    private static final Verbs[] verbsList = {HIDE,GET,GO,USE,LOOK,LOAD,SAVE,WAIT,HELP};
+    //private static final Verbs[] verbsList = {HIDE,GET,GO,USE,LOOK,LOAD,SAVE,WAIT,HELP};
 
-    public static InputData parseInput(String input){
+    public static InputData parseInput(String input) {
         String temp = input.trim();
         temp = temp.replaceAll("\\p{Punct}","");
-        for (Verbs verb : verbsList){
-            String verbString = verb.startVerb(temp);
-            if (verbString != null){
-                return new InputData(verb,verbString,temp.replaceAll(verbString,"").trim());
+        String[] inputList = StringUtils.split(temp, " ", 2);
+
+        try(InputStream inputStream = Parser.class.getResourceAsStream("verb.json")){
+            Object obj = new JSONParser().parse(new InputStreamReader(inputStream, "UTF-8"));
+            JSONObject verbs = (JSONObject) obj;
+            Iterator<String> keys = verbs.keySet().iterator();
+            while(keys.hasNext()) {
+                String key = keys.next();
+                //System.out.println(verbs.get(key).getClass());
+                if (verbs.get(key).toString().contains(inputList[0])){
+                    return new InputData(key, inputList[1]);
+                }
             }
-        }
+        }catch (IOException | ParseException e){
+            e.printStackTrace();
+        };
         return null;
     }
+//
+//    public static void main(String[] args) {
+//        System.out.println(parseInput("travel east").getVerb());
+//    }
+
 }
